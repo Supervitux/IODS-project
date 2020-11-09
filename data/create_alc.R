464932521b98b88088d463e013583acc7a0f2291
Supervitux <vitus.besel@web.de>
2020-11-09 11:34:16 +0200

    Exc3, first commit

---
+++ data/create_alc.R
@@ -0,0 +1,62 @@
+# Vitus Besel, 9.11.2020, this file contains Exc3, data wrangling
+# data from https://archive.ics.uci.edu/ml/machine-learning-databases/00320/
+
+# 3) read in data
+math <- read.csv("data/student-mat.csv", header = T, sep = ";")
+por <- read.csv("data/student-por.csv", header = T, sep = ";")
+
+# Explore dimension and structure
+dim(math)
+str(math)
+dim(por)
+str(por)
+
+# 4)
+# access the dplyr library
+library(dplyr)
+
+# common columns to use as identifiers
+join_by <- c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
+
+# join the two datasets by the selected identifiers
+math_por <- inner_join(math, por, by = join_by, suffix = c(".math", ".por"))
+
+# 5
+# create a new data frame with only the joined columns
+alc <- select(math_por, one_of(join_by))
+
+# the columns in the datasets which were not used for joining the data
+notjoined_columns <- colnames(math)[!colnames(math) %in% join_by]
+
+# print out the columns not used for joining
+notjoined_columns
+
+# for every column name not used for joining...
+for(column_name in notjoined_columns) {
+  # select two columns from 'math_por' with the same original name
+  two_columns <- select(math_por, starts_with(column_name))
+  # select the first column vector of those two columns
+  first_column <- select(two_columns, 1)[[1]]
+  
+  # if that first column vector is numeric...
+  if(is.numeric(first_column)) {
+    # take a rounded average of each row of the two columns and
+    # add the resulting vector to the alc data frame
+    alc[column_name] <- round(rowMeans(two_columns))
+  } else { # else if it's not numeric...
+    # add the first column vector to the alc data frame
+    alc[column_name] <- first_column
+  }
+}
+
+#6
+# define a new column alc_use by combining weekday and weekend alcohol use
+alc <- mutate(alc, alc_use = (Dalc + Walc) / 2)
+
+# define a new logical column 'high_use'
+alc <- mutate(alc, high_use = alc_use > 2)
+
+# Take a glimpse
+glimpse(alc)
+# Yes: 382 observations of 35 variables
+write.csv(alc, file = "data/alc.csv")
