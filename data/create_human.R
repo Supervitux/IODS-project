72970848e6cf81e94e57bc66fdfacd09556ad487
Supervitux <vitus.besel@web.de>
2020-11-23 11:36:54 +0200

    Data wrangling for exercise 6

--- data/create_human.R
+++ data/create_human.R
@@ -1,4 +1,4 @@
-# Vitus Besel, 16.11.2020, this file contains data wrangling for exercise 5
+# Vitus Besel, 16.11.2020, this file contains data wrangling for exercise 5 AND 6 !
 # data from https://archive.ics.uci.edu/ml/machine-learning-databases/00320/
 library(dplyr)
 # 2) read in data
@@ -26,3 +26,53 @@ human <- inner_join(hd, gii, by = "Country", suffix = c(".hd", ".gii"))
 str(human)
 
 write.csv(human, file = "data/human.csv")
+
+# Data wrangling continuation for exercise 6!
+# Data description:
+str(human)
+dim(human)
+# The data contains 195 obs. of 19 variables that contribute to the human development index HDI and Gender Inequality Index including themselves.
+# Therefore it contains information about birht/death rates, education etc.
+
+############### 6. Exercise ###################
+# 1. Mutate
+library(stringr)
+human <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt", sep  =",", header = T)
+#human <- mutate(human, GNI = as.numeric(GNI))
+str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric
+
+# 2.
+# columns to keep
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+
+# select the 'keep' columns
+human <- select(human, one_of(keep))
+
+# 3.
+# print out a completeness indicator of the 'human' data
+complete.cases(human)
+
+# print out the data along with a completeness indicator as the last column
+data.frame(human[-1], comp = complete.cases(human))
+
+# filter out all rows with NA values
+human_ <- filter(human, complete.cases(human))
+str(human_)
+# 4.
+# look at the last 10 observations of human
+tail(human, n = 10)
+
+# define the last indice we want to keep
+last <- nrow(human_) - 7
+
+# choose everything until the last 7 observations
+human_ <- human_[1:last, ]
+
+# add countries as rownames
+rownames(human_) <- human_$Country
+
+# 5. 
+# remove the Country variable
+human_ <- select(human_, -Country)
+# save
+write.csv(human_, file = "data/human.csv")
